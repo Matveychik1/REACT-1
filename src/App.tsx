9e90bcb84a7cdae29459163c9bc8b7b55426e3fe
CODER <CODER@rivne.itstep.org>
2026-01-16 10:59:17 +0200

    Перший репозиторій через консоль

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+import HomePage from "./pages/HomePage.tsx";
+import LoginPage from "./pages/LoginPage.tsx";
+
+function App() {
+
+    return (
+        <>
+
+            <HomePage/>
+            <LoginPage/>
+        </>
+    )
+}
+
+export default App
