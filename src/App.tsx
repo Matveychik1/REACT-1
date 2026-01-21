b9d1d0fb8ff8e195d8c79a9c841b4518b1e60d1f
CODER <CODER@rivne.itstep.org>
2026-01-21 10:10:13 +0200

    ADD nav

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,21 @@
 import './App.css'
 import HomePage from "./pages/HomePage.tsx";
 import LoginPage from "./pages/LoginPage.tsx";
+import {Route, Routes} from "react-router-dom";
+import MainLayout from "./componetns/layouts/MainLayout";
 
 function App() {
 
     return (
         <>
+            <Routes>
+                <Route path={"/"} element={<MainLayout/>}>
+                    <Route index element={<HomePage/>}/>
+                    <Route path={"login"} element={<LoginPage/>}/>
+                    <Route path={"test"} element={<MainLayout/>}/>
 
-            <HomePage/>
-            <LoginPage/>
+                </Route>
+            </Routes>
         </>
     )
 }
